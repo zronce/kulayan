d9169fba83804408076e57456b6981d95dc68a98
John Paul Banera <impaulintech@gmail.com>
2024-01-23 23:21:46 +0800

    Add vowel correction

--- kulitan/src/utils/latinizeVowels.ts
+++ kulitan/src/utils/latinizeVowels.ts
@@ -1,11 +1,21 @@
 export default function latinizeVowels(text: any) {
-	return text?.replace(/aa|oo|uu|ii/g, function (match: any) {
-		const replacements: any = {
-			aa: "á",
-			oo: "ó",
-			uu: "ú",
-			ii: "í",
-		};
-		return replacements[match];
-	});
+	return text?.replace(
+		/(aa|oo|uu|ii|ai|au|nganga|\b[bcdfghjklmnpqrstvwxyz]*a[^aeiou\s]a\b)/g,
+		function (match: any) {
+			const replacements: any = {
+				aa: "á ",
+				oo: "ó ",
+				uu: "ú ",
+				ii: "í ",
+				ai: "e ",
+				au: "o ",
+				nganga: "ngang ",
+			};
+			const replaced = match.replace(/a\b/, " ");
+			
+			return replacements[match]
+				? replacements[match] || match
+				: replaced || match;
+		},
+	);
 }
